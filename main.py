4c27436de9ce0905e03d34f154c25c5efb056c9d
kirjeo <jshay1949@gmail.com>
2022-04-08 13:33:35 -0400

    Fixed the DesktopCleaner so it would work with windows and catch attr errors

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ import os
 if os.name == 'posix':
     desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
 # For Windows
-else:
+elif os.name == 'nt':
     desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+# If the OS is not supported by Progam
+else:
+    print(f"OS not supported: {os.name}")
+
 os.chdir(desktop)
 list_of_shortcuts = os.listdir()
-
 
 for shortcut in list_of_shortcuts:
     if 'cpt' in shortcut.lower():
         if os.path.isfile(shortcut):
-            folder = re.search(r'JohnShay(.*?)-', shortcut).group(1)
-            shutil.move(shortcut,folder)
-            print(f'Moved {shortcut} to {folder}')
+            try:
+                folder = re.search(r'-(.*?)-', shortcut).group(1)
+                shutil.move(shortcut,folder)
+                print(f'Moved {shortcut} to {folder}')
+            except AttributeError:
+                print(f"File: \"{shortcut}\" is formatted wrong")
+
         else:
             pass
+
